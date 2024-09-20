ce65ec02d7f729eba6f0baa930115e6b0b093242
Teo Gontard <teo.gontard@gmail.com>
2024-09-20 11:20:53 +0200

    Added Full CRUD

--- src/main/java/com/jeido/film/controllers/CatalogApiController.java
+++ src/main/java/com/jeido/film/controllers/CatalogApiController.java
@@ -1,5 +1,6 @@
 package com.jeido.film.controllers;
 
+import com.jeido.film.dto.DirectorDtoReceive;
 import com.jeido.film.dto.DirectorDtoSend;
 import com.jeido.film.dto.FilmDtoReceive;
 import com.jeido.film.dto.FilmDtoSend;
@@ -8,12 +9,12 @@ import com.jeido.film.entities.Film;
 import com.jeido.film.services.DirectorService;
 import com.jeido.film.services.FilmService;
 import com.jeido.film.util.Genre;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -45,6 +46,12 @@ public class CatalogApiController {
         }
     }
 
+
+    @PostMapping("film")
+    public ResponseEntity<FilmDtoSend> addFilm(@Valid @RequestBody FilmDtoReceive filmDtoReceive) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(FilmDtoSend.of(filmService.save(filmDtoReceive)));
+    }
+
     @GetMapping("films")
     public ResponseEntity<List<FilmDtoSend>> films() {
         List<Film> films = filmService.findAll();
@@ -61,6 +68,22 @@ public class CatalogApiController {
         Film film = filmService.findById(id);
         FilmDtoSend filmDtoSend = FilmDtoSend.of(film);
         return ResponseEntity.ok(filmDtoSend);
+    }
+
+    @PutMapping("film")
+    public ResponseEntity<FilmDtoSend> updateFilm(@Validated @RequestBody FilmDtoReceive filmDtoReceive) {
+        return ResponseEntity.status(HttpStatus.OK).body(FilmDtoSend.of(filmService.save(filmDtoReceive)));
+    }
+
+    @DeleteMapping("film/{id}")
+    public ResponseEntity<List<FilmDtoSend>> deleteFilm(@PathVariable("id") UUID id) {
+        filmService.delete(id);
+        return films();
+    }
+
+    @PostMapping("director")
+    public ResponseEntity<DirectorDtoSend> addDirector(@Validated @RequestBody DirectorDtoReceive directorDtoReceive) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(DirectorDtoSend.of(directorService.save(directorDtoReceive), filmService));
     }
 
     @GetMapping("directors")
@@ -80,4 +103,16 @@ public class CatalogApiController {
         DirectorDtoSend directorDtoSend = DirectorDtoSend.of(director, filmService);
         return ResponseEntity.ok(directorDtoSend);
     }
+
+    @PutMapping("director")
+    public ResponseEntity<DirectorDtoSend> updateDirector(@Validated @RequestBody DirectorDtoReceive directorDtoReceive) {
+        return ResponseEntity.ok(DirectorDtoSend.of(directorService.save(directorDtoReceive), filmService));
+    }
+
+    @DeleteMapping("director/{id}")
+    public ResponseEntity<List<DirectorDtoSend>> deleteDirector(@PathVariable("id") UUID id) {
+        directorService.delete(id);
+        return directors();
+    }
+
 }
